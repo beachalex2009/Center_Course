6167451dfb82882861b3654420d0de9fd2dd6fef
Ahmed Tawfik <beach_alex2009@yahoo.com>
2023-11-29 21:11:58 +0200

    29-11-2023

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use App\Http\Middleware\CheckAge;
+use App\Http\Middleware\ChangeLang;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -38,6 +39,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             // CheckAge::class
+            ChangeLang::class
         ],
 
         'api' => [
